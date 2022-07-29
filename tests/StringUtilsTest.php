0760fb7b9ece10c13b28d505324a0f342ef7a362
m.vorobyev <max9680@mail.ru>
2022-07-29 15:30:20 +0300

    Add Assert in tests\StringUtilsTest.php

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -1,21 +1,21 @@
 <?php
 
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
+require("vendor/autoload.php");
+// $autoloadPath1 = __DIR__ . '/../../../autoload.php';
+// $autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+// if (file_exists($autoloadPath1)) {
+//     require_once $autoloadPath1;
+// } else {
+//     require_once $autoloadPath2;
+// }
 
 use function StringUtils\capitalize;
+use Webmozart\Assert\Assert;
 
-if (StringUtils\capitalize('hello') !== 'Hello') {
-    throw new \Exception('Функция работает неверно!');
-}
+Assert::eq(StringUtils\capitalize('hello'), 'Hello');
 
-if (StringUtils\capitalize('') !== '') {
-    throw new \Exception('Функция работает неверно!');
-}
+Assert::eq(StringUtils\capitalize(''), '');
+
+Assert::eq(StringUtils\capitalize('890'), '890');
 
 echo 'Все тесты пройдены!';
